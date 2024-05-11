c8f6341dc337d7e9324907d48c6aa0d2549c5bb2
Kunal <kunalrode2003@gmail.com>
2024-05-11 07:13:08 +0530

    first

---
+++ Array03.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    // vector<int>arr;
+    // cout<<arr.size()<<endl;
+    
+
+    // arr.push_back(5);
+    // arr.push_back(7);
+    // arr.push_back(3);
+    // arr.push_back(9);
+    // arr.push_back(1);
+    // cout<<arr.capacity()<<endl;
+    // for(int i=0;i<arr.size();i++){
+    //     cout<<arr[i]<<" ";
+    // }
+
+    // unique element
+    int n;
+    cout<<"Enter the size of array"<<endl;
+    cin>>n;
+
+    vector<int>arr
+}
